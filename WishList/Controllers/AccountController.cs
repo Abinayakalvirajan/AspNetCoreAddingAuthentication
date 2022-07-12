151f55a201b4d6ec17483cfabd70fb4d4677b191
Abinaya <88155924+Abinayakalvirajan@users.noreply.github.com>
2022-07-12 11:14:38 +0530

    Update AccountController.cs

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
@@ -7,5 +9,7 @@ namespace WishList.Controllers
     public class AccountController : Controller
     {
 
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
     }
 }
