7fca874c1b1a3ec38900b15955fe9c493a7cb1bd
Abinaya <88155924+Abinayakalvirajan@users.noreply.github.com>
2022-07-12 10:28:26 +0530

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -8,7 +8,11 @@ using WishList.Models;
 
 namespace WishList
 {
-public void ConfigureServices(IServiceCollection services)
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
@@ -16,11 +20,18 @@ public void ConfigureServices(IServiceCollection services)
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
             }
-
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
         }
     }
+}
